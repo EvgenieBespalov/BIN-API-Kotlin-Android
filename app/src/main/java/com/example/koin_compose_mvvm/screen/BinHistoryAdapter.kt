070d92b3321e44b31bee307bde4798d34dd47d62
EvgenieBespalov <evg.bespaloff@yandex.ru>
2023-04-02 21:03:34 +0700

    Добавил адаптер для списка

---
+++ app/src/main/java/com/example/koin_compose_mvvm/screen/BinHistoryAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.koin_compose_mvvm.screen
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.koin_compose_mvvm.R
+import com.example.koin_compose_mvvm.domain.entity.MainData
+import java.util.Calendar.getInstance
+
+class BinHistoryAdapter(private val userClickListener: (MainData) -> Unit) : RecyclerView.Adapter<HistoryViewHolder>() {
+
+    var mainData: List<MainData> = emptyList()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.bin_history_item, parent, false)
+        return HistoryViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: HistoryViewHolder, position: Int) {
+        holder.bind(mainData[position], userClickListener)
+    }
+
+    override fun getItemCount(): Int =
+        mainData.size
+}
+
+class HistoryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    private val binText get() = itemView.findViewById<TextView>(R.id.binHistoryItemTextView)
+    private val dateEmailText get() = itemView.findViewById<TextView>(R.id.dateHistoryItemTextView)
+
+    fun bind(mainData: MainData, userClickListener: (MainData) -> Unit) {
+        binText.setText(mainData.bin)
+        dateEmailText.setText(mainData.date)
+
+
+        /*itemView.setOnClickListener {
+            userClickListener(user)
+        }*/
+    }
+}
--- app/src/main/java/com/example/koin_compose_mvvm/screen/BinHistoryFragment.kt
+++ app/src/main/java/com/example/koin_compose_mvvm/screen/BinHistoryFragment.kt
@@ -8,7 +8,9 @@ import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.koin_compose_mvvm.R
+import com.example.koin_compose_mvvm.domain.entity.MainData
 import com.example.koin_compose_mvvm.presentation.BinHistoryUiState
 import com.example.koin_compose_mvvm.presentation.BinHistoryViewModel
 import com.example.koin_compose_mvvm.presentation.FindBinUiState
@@ -18,26 +20,61 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class BinHistoryFragment : Fragment(R.layout.fragment_bin_history)  {
     private val progressBar get() = requireView().findViewById<ProgressBar>(R.id.progressBar)
     private val errorText get() = requireView().findViewById<TextView>(R.id.errorText)
+    private val binHistoryContent get() = requireView().findViewById<LinearLayout>(R.id.binHistoryContent)
+
+
+    private var binHistory: RecyclerView? = null
 
     private val viewModel: BinHistoryViewModel by viewModel()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.state.observe(viewLifecycleOwner, ::handleState)
-        viewModel.initial()
+        binHistory = view.findViewById(R.id.historyRecyclerView)
+        binHistory?.adapter = BinHistoryAdapter(::handleLoanClick)
 
-        //setListeners()
+        viewModel.state.observe(viewLifecycleOwner, ::handleState)
+
+        viewModel.loadData()
+    }
+
+    private fun handleLoanClick(mainData: MainData) {
+        //mainActivity.openUserDetails(mainData.id)
     }
 
     private fun handleState(state: BinHistoryUiState) {
         when (state) {
-            BinHistoryUiState.Initial    -> showContent()
+            BinHistoryUiState.Initial    -> Unit
+            BinHistoryUiState.Loading    -> showProgress()
+            is BinHistoryUiState.Content -> showContent(state.mainData)
+            is BinHistoryUiState.Error   -> showError(state.message)
         }
     }
 
-    private fun showContent() {
+    private fun showContent(listBin: List<MainData>) {
         progressBar.isVisible = false
         errorText.isVisible = false
+        binHistoryContent.isVisible = true
+        (binHistory?.adapter as? BinHistoryAdapter)?.mainData = listBin
+    }
+
+    private fun showProgress() {
+        progressBar.isVisible = true
+        errorText.isVisible = false
+        binHistoryContent.isVisible = false
+    }
+
+    private fun showError(message: String?) {
+        errorText.isVisible = true
+        progressBar.isVisible = false
+        binHistoryContent.isVisible = false
+
+        errorText.text = message ?: requireContext().getText(R.string.unknown_error)
+    }
+
+    override fun onDestroy() {
+        binHistory?.adapter = null
+        binHistory = null
+        super.onDestroy()
     }
 }
